2c4e19b8448b25c7a23ebaecad224420ce290061
zunayed <zunayed.arp@gmail.com>
2025-09-03 15:22:03 +0200

    created new components History

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,44 +1,20 @@
 import { useState } from "react";
 import Board from "./Board";
+import History from "./History";
 
 export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [xIsNext, setXIsNext] = useState(true);
   const [currentMove, setCurrentMove] = useState(0);
 
-  console.log(history);
-
-  const currentSquares = history[history.length - 1];
-
-  console.log(`history length ${history.length - 1}`);
-
-  console.log(`current squares ${currentSquares}`);
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
     setXIsNext(!xIsNext);
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
-    setHistory([nextHistory]);
+    setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
   }
-
-  function jumpTo(move) {
-    setCurrentMove(move);
-    setXIsNext(move % 2 == 0);
-  }
-
-  const moves = history.map((squares, move) => {
-    let description;
-    if (move > 0) {
-      description = `Go to the move # ${move}`;
-    } else {
-      description = `Start the Game`;
-    }
-    return (
-      <li key={move}>
-        <button onClick={() => jumpTo(move)}>{description}</button>
-      </li>
-    );
-  });
 
   return (
     <div>
@@ -46,7 +22,11 @@ export default function Game() {
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div>
-        <ol>{moves}</ol>
+        <History
+          history={history}
+          setCurrentMove={setCurrentMove}
+          setXIsNext={setXIsNext}
+        />
       </div>
     </div>
   );
---
+++ src/components/History.jsx
@@ -0,0 +1,25 @@
+export default function History({ history, setCurrentMove, setXIsNext }) {
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = `Go to the move # ${move}`;
+    } else {
+      description = `Start the Game`;
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  function jumpTo(move) {
+    setCurrentMove(move);
+    setXIsNext(move % 2 === 0);
+  }
+  return (
+    <>
+      <ol>{moves}</ol>
+    </>
+  );
+}
